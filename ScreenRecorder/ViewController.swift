1d66d0a85354c5f11735facfd50482574546ac6b
Saifullah ilyas <saifullah@mobinspire.me>
2020-10-20 00:11:35 +0500

    pre defined system recorder picker added

--- ScreenRecorder/ViewController.swift
+++ ScreenRecorder/ViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import ReplayKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       // let broadcastPicker = RPSystemBroadcastPickerView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        let broadCastPicker = RPSystemBroadcastPickerView.init(frame: CGRect(x: view.center.x, y: view.center.y, width: view.frame.width/10, height:  view.frame.width/10))
+        broadCastPicker.showsMicrophoneButton = false
+        
+        //broadcastPicker.preferredExtension = "com.CodeRecipe.ScreenRecorder.broadcast.extension"
+        view.addSubview(broadCastPicker)
+        
+        
         // Do any additional setup after loading the view.
     }
 
 
 }
+
 
