56a966a1f9500b42efcff78069412712e04fc042
Saifullah ilyas <saifullah@mobinspire.me>
2020-10-19 23:45:08 +0500

    init Git

---
+++ ScreenRecorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreenRecorder
+//
+//  Created by Saifullah on 19/10/2020.
+//  Copyright © 2020 CodeRecipe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
